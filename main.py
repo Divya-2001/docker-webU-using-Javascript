db0fd4fdb2883a78bf89c48fa0432ffe5428ea23
Divya-2001 <60807380+Divya-2001@users.noreply.github.com>
2021-06-24 19:11:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,5 +33,6 @@ elif ( ("container" in cmd) and ("docker" in cmd) or ( ("os" in cmd) or ("contai
 3. systemctl start httpd
 4. setenforce 0 (stop selinux)
 4. vim /etc/group (set apache id to 0)
-'''
+
 5. vim /etc/sudoers (set apache ALL=(ALL) NOPASSWD: ALL)
+'''
